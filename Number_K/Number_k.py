4b0c71746951522f910b504c050351692e24bf3f
cheol-95 <rkdcjf0122@gmail.com>
2020-04-02 02:25:54 +0900

    add example lambda

--- Number_K/Number_k.py
+++ Number_K/Number_k.py
@@ -1,9 +1,15 @@
-def solution(nList, cmds):
-    result = []
-    for i in range(len(cmds)):
-        result.append(sorted(nList[cmds[i][0]-1:cmds[i][1]])[cmds[i][2]-1])
-    print(result)
+def solution(array, commands):
+    answer = []
+    for i in range(len(commands)):
+        answer.append(sorted(array[commands[i][0]-1:commands[i][1]])[commands[i][2]-1])
+    return answer
 
-nList = [1,5,2,6,3,7,4]
-cmds = [[2,5,3],[4,4,1],[1,7,3]]
-solution(nList, cmds)
+array = [1, 5, 2, 6, 3, 7, 4]
+commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+print(solution(array, commands))
+
+
+'''
+def solution(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+'''

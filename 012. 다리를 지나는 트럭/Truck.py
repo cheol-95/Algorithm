4d2765aa9cc72d02ac668a4ecaea0a8d2fa2c914
cheol-95 <rkdcjf0122@gmail.com>
2020-04-15 18:00:20 +0900

    Add 코드 설명

--- 012. 다리를 지나는 트럭/Truck.py
+++ 012. 다리를 지나는 트럭/Truck.py
@@ -1,18 +1,18 @@
 def solution(bridge_length, weight, truck_weights):
-    answer = 0
-    on = []
-    for i in truck_weights:
+    answer = 0 # 총 소요시간
+    on = [] # 다리에 올라와있는 트럭 큐
+    for i in truck_weights: # 트럭 수 만큼 반복
         while True:
-            if len(on) == bridge_length:
-                on.pop()
-            if(sum(on) + i) <= weight:
-                on.insert(0, i)
-                answer += 1
+            if len(on) == bridge_length: # 다리에 트럭이 가득 올라와있다면
+                on.pop() # 가장 먼저 올라온 트럭 pop
+            if(sum(on) + i) <= weight: # 다리위의 트럭 총 무게 + 다음 트럭무게 <= 최대하중
+                on.insert(0, i) # 다음 트럭 진입
+                answer += 1 # 소요시간 + 1
                 break
-            else:
-                on.insert(0, 0)
-                answer += 1
-    answer = answer + bridge_length
+            else: # 하중 제한때문에 다음 트럭을 올릴수가 없다면
+                on.insert(0, 0) # 모든 트럭 1만큼 전진
+                answer += 1 # 소요시간 + 1
+    answer = answer + bridge_length # 마지막 트럭의 이동시간을 고려하지 않았으므로 다리의 길이만큼 더해준다
     return answer
 
 

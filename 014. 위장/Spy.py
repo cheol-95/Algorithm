bcd9f12ca8a45b4d23ad75e5ed066732bd2dbf00
cheol-95 <rkdcjf0122@gmail.com>
2020-04-17 01:03:28 +0900

    Add other planation

--- 014. 위장/Spy.py
+++ 014. 위장/Spy.py
@@ -1,22 +1,11 @@
+from collections import Counter
 def solution(clothes):
-    li = []
-    cnt = []
-    for i, v in clothes:
-        li.append(v)
-
-    for i in range(len(li)):
-        tmp = li[i]
-        if type(tmp) == int:
-            continue
-        cnt.append(li.count(tmp))
-        for v in range(cnt[i]):
-            li[li.index(tmp)] = i
-    li[1] = 1
-    for i in cnt:
-        li[1] *= i
-
-    return sum(cnt) + li[1]
-
+    answer = 1
+    count = Counter(list(v for i,v in clothes))
+    for i in count:
+        answer *= count[i]+1
+    return answer-1
 
 clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
+# clothes = [["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]
 print(solution(clothes))

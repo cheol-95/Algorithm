381eaf09a31ebecaabad38e690c774edd0d13cec
cheol-95 <rkdcjf0122@gmail.com>
2020-04-24 23:21:50 +0900

    기능 개발

---
+++ 019. 기능개발/Function Development.py
@@ -0,0 +1,18 @@
+def solution(progresses, speeds):
+    answer = []
+    while progresses:
+        cnt = 0
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+        while progresses[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
+            if not progresses:
+                break
+        if cnt != 0:
+            answer.append(cnt)
+    return answer
+
+progresses, speeds = [93,30,55], [1,30,5]
+print(solution(progresses, speeds))

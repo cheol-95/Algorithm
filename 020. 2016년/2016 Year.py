3383de6fccf230291ee375d79494ea0147cf1aa4
cheol-95 <rkdcjf0122@gmail.com>
2020-04-28 00:19:05 +0900

    Update explanation

--- 020. 2016년/2016 Year.py
+++ 020. 2016년/2016 Year.py
@@ -1,50 +1,14 @@
 def solution(a, b):
-    answer = ''
-    cnt = 1
+    day = ['WED','THU','FRI','SAT','SUN','MON','TUE']
+    aN = int(a/2)
     if a == 1:
-        cnt += 0
-    elif a == 2:
-        cnt += 29
-    elif a == 3:
-        cnt += 60
-    elif a == 4:
-        cnt += 90
-    elif a == 5:
-        cnt += 121
-    elif a == 6:
-        cnt += 151
-    elif a == 7:
-        cnt += 182
-    elif a == 8:
-        cnt += 213
-    elif a == 9:
-        cnt += 243
-    elif a == 10:
-        cnt += 274
-    elif a == 11:
-        cnt += 304
-    elif a == 12:
-        cnt += 335
-    cnt += b
+        pass
+    elif a%2 == 1:
+        b += aN*31 + (aN-1)*30 + 29 +1
+    elif a%2 == 0:
+        b += (aN-1)*31 + aN*30 + 29 +1
+    return day[b%7]
 
-    res = cnt % 7
-
-    if res == 0:
-        answer = 'SUN'
-    if res == 1:
-        answer = 'SAT'
-    if res == 2:
-        answer = 'FRI'
-    if res == 3:
-        answer = 'SAT'
-    if res == 4:
-        answer = 'SUN'
-    if res == 5:
-        answer = 'MON'
-    if res == 6:
-        answer = 'TUE'
-    return answer
-
-a, b = 5, 24
+a, b = 5,24
 print(solution(a,b))
 

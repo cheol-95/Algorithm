eabc1887b0bae93d54337e6b5d70123da59dbac8
cheol-95 <rkdcjf0122@gmail.com>
2020-04-30 19:02:47 +0900

    멀쩡한 사각형

--- 022. 멀쩡한 사각형/Ractangle.py
+++ 022. 멀쩡한 사각형/Ractangle.py
@@ -3,13 +3,11 @@ def solution(w,h):
     tmp = gcd(w,h)
     if w > h: # h를 큰 수로 변경
         w,h = h,w
-
     ww = w//tmp
     hh = h//tmp
     a=hh/2
     if str(type(a)) == "<class 'float'>":
         a = int(a)+1
-
     answer = w*h-(a*ww*tmp)
     if answer == 1:
         return 0

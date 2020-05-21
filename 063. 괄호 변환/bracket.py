87a2e0db96768760957fa39117d06e865d118622
cheol-95 <rkdcjf0122@gmail.com>
2020-05-22 03:07:23 +0900

    괄호 변환

--- 063. 괄호 변환/bracket.py
+++ 063. 괄호 변환/bracket.py
@@ -1,36 +1,24 @@
 def solution(p):
-    cnt1 = 0
-    cnt2 = 0
-    u = ''
-    stack = []
-    for i,char in enumerate(p):
-        if stack and stack[-1] == '(' and char == ')':
-            stack.pop()
+    if not p:
+        return ''
+    cnt1, cnt2 = 0, 0
+    proper = True
+    for i,v in enumerate(p):
+        if v == '(':
+            cnt1 += 1
         else:
-            stack.append(char)
-        if char == '(':
-            u, cnt1 = u + '(', cnt1 + 1
-        else:
-            u, cnt2 = u + ')', cnt2 + 1
-        if cnt1 and cnt1 == cnt2:
-            if stack: # 올바르지 않은 문자열
-                tmp1 = ''
-                for v in u[1:len(u)-1]:
-                    if v == '(':
-                        tmp1 += ')'
-                    else:
-                        tmp1 += '('
-                if len(u)==2:
-                    return '(' + tmp1 + ')'
-                return '(' + solution(p[i+1:]) + ')' + tmp1
-            else: #올바른 괄호 문자열
-                if not p[i+1:] or len(p)==2:
-                     return u
-                return u+solution(p[i+1:])
-
-
-# p = "(()())()"
-# p = ')('
-# p = "()))((()"
-p = '(((())))'
-print(solution(p))
+            cnt2 += 1
+            if cnt1 < cnt2:
+                proper = False
+        if cnt1 == cnt2:
+            if proper == False:
+                tmp = '(' + solution(p[i+1:len(p)]) + ')'
+                for j in p[1:i]:
+                    if j == '(':
+                        tmp += ')'
+                    if j == ')':
+                        tmp += '('
+                return tmp
+            else:
+                tmp = p[:i+1] + solution(p[i+1:len(p)])
+                return tmp

041be9d7f5045a6d87291d204b44fe625ddc408a
cheol-95 <rkdcjf0122@gmail.com>
2020-05-28 20:37:37 +0900

    JadenCase

---
+++ 078. JadenCase 문자열 만들기/JadenCase.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = ''
+    s += ' '
+    cnt = 0
+    while cnt < len(s):
+        if s[cnt] == ' ':
+            answer += ' '
+            cnt += 1
+        else:
+            answer += s[cnt].upper()
+            cnt += 1
+            while s[cnt] != ' ':
+                answer += s[cnt].lower()
+                cnt += 1
+    return answer[:-1]
+
+s = '3people unFollowed me'
+# s = 'for the last  week	'
+print(solution(s))

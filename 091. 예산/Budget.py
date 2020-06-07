522e820087ff64b6a85db1002eb4f1f455cc6df1
cheol-95 <rkdcjf0122@gmail.com>
2020-06-08 00:00:56 +0900

    예산

--- 091. 예산/Budget.py
+++ 091. 예산/Budget.py
@@ -1,4 +1,20 @@
 def solution(d, budget):
+    answer = 0
+    sum = 0
+    d.sort()
+    for i in d:
+        sum += i
+        answer += 1
+        if sum > budget:
+            return answer -1
+
+d, budget = [1, 3, 2, 5, 4], 9
+# d, budget = [2,2,3,3], 10
+print(solution(d, budget))
+
+
+'''
+def solution(d, budget):
     cnt = 1
     while True:
         tmp_d = [_ for _ in d]
@@ -13,7 +29,4 @@ def solution(d, budget):
             tmp_d.remove(min(tmp_d))
         if tmp > budget:
             return cnt-1
-
-d, budget = [1, 3, 2, 5, 4], 9
-d, budget = [2,2,3,3], 10
-print(solution(d, budget))
+        '''

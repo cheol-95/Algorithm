f8b83d252d7a8072a3dc4965eebc8269675c0daa
cheol-95 <rkdcjf0122@gmail.com>
2020-06-09 16:00:06 +0900

    비밀지도

---
+++ 093. 비밀지도/Secret Map.py
@@ -0,0 +1,11 @@
+def solution(n, arr1, arr2):
+    for i in range(len(arr1)):
+        sum = bin(arr1[i] | arr2[i])[2:].replace('1','#').replace('0',' ')
+        if len(sum) < n:
+            sum = ' '*(n-len(sum))+sum
+        arr1[i] = sum
+    return arr1
+n, arr1, arr2 = 5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]
+n, arr1, arr2 = 6, [46, 33, 33, 22, 31, 50], [27, 56, 19, 14, 14, 10]
+print(solution(n,arr1,arr2))
+tmp = bin(30)[2:]

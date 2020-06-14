52e6cb93a74dca0b6cd74c30e4143470964aa334
cheol-95 <rkdcjf0122@gmail.com>
2020-06-14 23:43:14 +0900

    방금 그곡

--- 098. 방금 그 곡/music.py
+++ 098. 방금 그 곡/music.py
@@ -1,17 +1,16 @@
 def solution(m, musicinfos):
     candidate = []
+    m = m.replace('C#','H').replace('D#','I').replace('F#','J').replace('G#','K').replace('A#','L')
     for i in musicinfos:
         tmp = i.split(',')
         length = (int(tmp[1][:2])*60+int(tmp[1][3:]) - (int(tmp[0][:2])*60+int(tmp[0][3:])))
-        music = (tmp[3]*(length+tmp[3].count('#')))[:length+tmp[3].count('#')]
-        print(music)
-        if m in music and music[music.index(m[0])+len(m)] != '#':
+        tmp[3] = tmp[3].replace('C#','H').replace('D#','I').replace('F#','J').replace('G#','K').replace('A#','L')
+        music = (tmp[3]*(length))[:length]
+        if m in music:
             candidate.append([tmp[2],length])
     if len(candidate) == 0:
         return '(None)'
     return sorted(candidate,key=lambda x:x[1],reverse=True)[0][0]
 
-m, musicinfos = "ABCDEFG", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]
-# m, musicinfos = "CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]
-# m, musicinfos = "ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]
+m, musicinfos = "ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]
 print(solution(m, musicinfos))

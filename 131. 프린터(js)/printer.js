02c31836cc977a011f1ba86a88ded9c3813a6f65
cheol-95 <rkdcjf0122@gmail.com>
2021-03-01 16:30:18 +0900

    프린터(js)

--- 131. 프린터(js)/printer.js
+++ 131. 프린터(js)/printer.js
@@ -1,39 +1,17 @@
-function solution(numbers) {
-  var answer = 0;
-  var n = numbers.split('');
-  var nums = new Set();
-  permutation(n, '');
-
-  function permutation(a, s) {
-    if (s.length > 0) {
-      if (nums.has(Number(s)) === false) {
-        nums.add(Number(s));
-        // console.log(Number(s));
-        if (chkPrime(Number(s))) {
-          answer++;
-        }
+function solution(priorities, location) {
+  let rank = 0;
+  while (1) {
+    if (priorities[0] === Math.max.apply(null, priorities)) {
+      priorities.splice(0, 1);
+      rank += 1;
+      if (location === 0) {
+        return rank;
+      } else {
+        location -= 1;
       }
-    }
-    if (a.length > 0) {
-      for (var i = 0; i < a.length; i++) {
-        var t = a.slice(0);
-        t.splice(i, 1);
-        console.log(t, s + a[i]);
-        permutation(t, s + a[i]);
-      }
+    } else {
+      priorities = [...priorities.slice(1), priorities[0]];
+      location = location === 0 ? priorities.length - 1 : location - 1;
     }
   }
-
-  function chkPrime(num) {
-    if (num < 2) return false;
-    if (num === 2) return true;
-    for (var i = 2; i <= Math.sqrt(num); i++) {
-      if (num % i === 0) return false;
-    }
-    // console.log(`prime = ${num}`);
-    return true;
-  }
-  return answer;
 }
-
-console.log('solution(1781): ', solution('178'));

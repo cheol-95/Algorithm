2470462feedbe29e9521dc4226602ee2dd8904bb
cheol-95 <rkdcjf0122@gmail.com>
2021-03-24 19:10:12 +0900

    140. 쿼드 압축

--- 140. 쿼드 압축(python)/quad.py
+++ 140. 쿼드 압축(python)/quad.py
@@ -1,36 +1,20 @@
-def check_same(arr, sub_y, sub_x, size):
-    num = arr[sub_y][sub_x]
-    for y in range(sub_y, sub_y+size):
-        for x in range(sub_x, sub_x+size):
-            if arr[y][x] not in [num, 5] :
-                return False
-    return True
-
-def change_arr(arr, sub_y, sub_x, size):
-    num = arr[sub_y][sub_x]
-    for y in range(sub_y, sub_y+size):
-        for x in range(sub_x, sub_x+size):
-            arr[y][x] = 5
-    arr[sub_y][sub_x] = num
-    return arr
-
 def solution(arr):
-    answer = [0, 0]
-    size = 2
-    length = len(arr)
+    def check(y, x, size):
+        if size == 1:
+            return [0, 1] if arr[y][x] == 1 else [1, 0]
 
-    while size <= length:
-        for y in range(0, length, size):
-            for x in range(0, length, size):
-                if check_same(arr, y, x, size):
-                    arr = change_arr(arr, y, x, size)
-        size *= 2
+        next_size = size // 2
+        one = check(y, x, next_size)
+        two = check(y, x + next_size, next_size)
+        three = check(y + next_size, x, next_size)
+        four = check(y + next_size, x + next_size, next_size)
 
-    for row in arr:
-        answer[0] += row.count(0)
-        answer[1] += row.count(1)
+        if one == two == three == four:
+            return one
+        else:
+            return list(map(sum, zip(one, two, three, four)))
 
-    return answer
+    return check(0, 0, len(arr))
 
 arr = [[1,1,1,1,1,1,1,1],[0,1,1,1,1,1,1,1],[0,0,0,0,1,1,1,1],[0,1,0,0,1,1,1,1],[0,0,0,0,0,0,1,1],[0,0,0,0,0,0,0,1],[0,0,0,0,1,0,0,1],[0,0,0,0,1,1,1,1]]
 # arr = [[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]]

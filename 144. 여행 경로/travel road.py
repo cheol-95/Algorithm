fc9340542e89798a3b0a5aaf2c086e4af19768fa
cheol-95 <rkdcjf0122@gmail.com>
2021-03-30 15:03:03 +0900

    144. 여행 경로

--- 144. 여행 경로/travel road.py
+++ 144. 여행 경로/travel road.py
@@ -1,24 +1,45 @@
+# import collections
+# def solution(tickets):
+#     routes = collections.defaultdict(list)
+#     for start, destination in tickets:
+#         routes[start].append(destination)
+#
+#     for route in routes:
+#         routes[route].sort(reverse=True)
+#
+#     stack = ["ICN"]
+#     path = []
+#
+#     while stack:
+#         top = stack[-1]
+#
+#         if top not in routes or len(routes[top]) == 0:
+#             path.append(stack.pop())
+#         else:
+#             stack.append(routes[top].pop())
+#
+#     return path[::-1]
+
 import collections
 def solution(tickets):
-    routes = collections.defaultdict(list)
-    for start, destination in tickets:
-        routes[start].append(destination)
-
-    for route in routes:
-        routes[route].sort(reverse=True)
+    ticket = collections.defaultdict(list)
+    for a, b in tickets:
+        ticket[a].append(b)
+        ticket[a].sort(reverse=True)
 
     stack = ["ICN"]
-    path = []
-
+    result = []
     while stack:
-        top = stack[-1]
+        start = stack[-1]
 
-        if top not in routes or len(routes[top]) == 0:
-            path.append(stack.pop())
+        if start not in ticket or len(ticket[start]) == 0:
+            result.append(stack.pop())
         else:
-            stack.append(routes[top].pop())
+            stack.append(ticket[start].pop())
 
-    return path[::-1]
+    return result[::-1]
 
+
+# tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
 tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
 print(solution(tickets))

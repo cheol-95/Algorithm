87e09b8cd2ba7024bd60843b6a18b2aab942677e
cheol-95 <rkdcjf0122@gmail.com>
2021-04-06 14:13:15 +0900

    100. 가운데를 말해요/

--- 100. 가운데를 말해요/Center Number.py
+++ 100. 가운데를 말해요/Center Number.py
@@ -3,5 +3,9 @@ N = int(input())
 heap = []
 for i in range(N):
     heapq.heappush(heap, int(input()))
-    tmp = heapq.nlargest((i+1)//2+1, heap)
-    print(tmp[len(tmp)-1])
+    # tmp = heapq.nlargest((i+1)//2+1, heap)
+    # print(tmp[-1])
+    tmp = heapq.nsmallest((i + 1) // 2 + 1, heap)
+    print(tmp)
+    print(tmp[-1])
+

a85a798e9b83bb1e7d21fa94a99dff672d1a4b39
cheol-95 <rkdcjf0122@gmail.com>
2021-04-07 20:07:51 +0900

    140. 쿼드 압축(python)/

--- 140. 쿼드 압축(python)/quad.py
+++ 140. 쿼드 압축(python)/quad.py
@@ -1,22 +1,24 @@
+def check(y, x, size):
+    if size == 1:
+        return [0, 1] if arr[y][x] == 1 else [1, 0]
+
+    next_size = size // 2
+    one = check(y, x, next_size)
+    two = check(y, x + next_size, next_size)
+    three = check(y + next_size, x, next_size)
+    four = check(y + next_size, x + next_size, next_size)
+
+    if one == two == three == four:
+        return one
+    else:
+        print(zip(one, two, three, four))
+        print(list(map(sum, zip(one, two, three, four))))
+        return list(map(sum, zip(one, two, three, four)))
+
 def solution(arr):
-    def check(y, x, size):
-        if size == 1:
-            return [0, 1] if arr[y][x] == 1 else [1, 0]
-
-        next_size = size // 2
-        one = check(y, x, next_size)
-        two = check(y, x + next_size, next_size)
-        three = check(y + next_size, x, next_size)
-        four = check(y + next_size, x + next_size, next_size)
-
-        if one == two == three == four:
-            return one
-        else:
-            return list(map(sum, zip(one, two, three, four)))
-
     return check(0, 0, len(arr))
 
 arr = [[1,1,1,1,1,1,1,1],[0,1,1,1,1,1,1,1],[0,0,0,0,1,1,1,1],[0,1,0,0,1,1,1,1],[0,0,0,0,0,0,1,1],[0,0,0,0,0,0,0,1],[0,0,0,0,1,0,0,1],[0,0,0,0,1,1,1,1]]
 # arr = [[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]]
-arr =  [[0,0],[0,0]]
+# arr =  [[0,0],[0,0]]
 print(solution(arr))

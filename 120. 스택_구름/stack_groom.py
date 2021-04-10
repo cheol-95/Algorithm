b3666d08eb872d76154faa8b06fd07f6c2a22431
cheol-95 <rkdcjf0122@gmail.com>
2021-04-11 03:26:35 +0900

    120. 스택_구름/

--- 120. 스택_구름/stack_groom.py
+++ 120. 스택_구름/stack_groom.py
@@ -1,21 +1,47 @@
-stack = []
-input_size = int(input())
+# stack = []
+# input_size = int(input())
+#
+# for i in range(input_size):
+#   command=int(input())
+#   if command == 1:
+#     try:
+#       stack.pop()
+#     except:
+#       print('underflow')
+#   elif command == 0:
+#     tmp = input()
+#     if len(stack) == 10:
+#       print('overflow')
+#     else:
+#       stack.append(tmp)
+#   else:
+#     break
+#
+# for i in stack:
+#   print(i, end=' ')
 
-for i in range(input_size):
-  command=int(input())
-  if command == 1:
-    try:
-      stack.pop()
-    except:
-      print('underflow')
-  elif command == 0:
-    tmp = input()
+
+N = int(input())
+stack = []
+
+for _ in range(N):
+  control = int(input())
+
+  if control == 0: # 푸시
     if len(stack) == 10:
       print('overflow')
-    else:
-      stack.append(tmp)
-  else:
+      continue
+    stack.append(input())
+
+  elif control == 1: # 팝
+    if len(stack) == 0:
+      print('underflow')
+      continue
+    stack.pop()
+
+  else: # 종료
     break
 
 for i in stack:
   print(i, end=' ')
+

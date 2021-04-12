5b0417d17eabc47a48687e2c953c98a500ec700e
cheol-95 <rkdcjf0122@gmail.com>
2021-04-12 23:40:12 +0900

    143. 등굣길/

--- 143. 등굣길/road to school.py
+++ 143. 등굣길/road to school.py
@@ -1,32 +1,18 @@
 def solution(m, n, puddles):
-    load = [[0] * m for _ in range(n)]
-    load[0][0] = 1
+    puddles = list(map(lambda x: x[::-1], puddles))
+    result = [[0] * (m+1) for _ in range(n+1)]
+    result[1][1] = 1
 
-    for y in range(0, n):
-        for x in range(0, m):
-            if y == x == 0:
+    for y in range(1, n+1):
+        for x in range(1, m+1):
+            if y == 1 and x == 1:
                 continue
-            if [x+1, y+1] in puddles:
-                load[y][x] = 0
+            if [y, x] in puddles:
+                result[y][x] = 0
             else:
-                load[y][x] = load[y][x-1] + load[y-1][x]
+                result[y][x] = result[y-1][x] + result[y][x-1]
 
-    return load[n-1][m-1] % 1000000007
-
-# def solution(m, n, puddles):
-#     load = [[1] * m for _ in range(n)]
-#     for puddle_x, puddle_y in puddles:
-#         load[puddle_y - 1][puddle_x - 1] = 0
-#
-#     for y in range(n - 2, -1, -1):
-#         for x in range(m - 2, -1, -1):
-#             if load[y][x]:
-#                 load[y][x] = load[y + 1][x] + load[y][x + 1]
-#
-#     for i in load:
-#         print(i)
-#     return load[0][0]
-
+    return result[n][m] % 1000000007
 
 
 m, n, puddles = 4, 3, [[2, 2]]

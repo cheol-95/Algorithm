7fb2eed86be517e7adf63c9f8e776947094f47b9
cheol-95 <rkdcjf0122@gmail.com>
2021-05-14 15:59:04 +0900

    187. 1로 만들기 - 풀이 참고/

--- 187. 1로 만들기/Make One.py
+++ 187. 1로 만들기/Make One.py
@@ -1,21 +1,30 @@
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
+#
+# N = int(input())
+# D = [0] * (N + 1)
+# t = 2
+#
+# while t <= N:
+#     candidate = 1000
+#     if t % 3 == 0:
+#         candidate = D[t // 3] + 1 if D[t // 3] + 1 < candidate else candidate
+#     if t % 2 == 0:
+#         candidate = D[t // 2] + 1 if D[t // 2] + 1 < candidate else candidate
+#     if D[t-1]:
+#         candidate = D[t - 1] + 1 if D[t - 1] + 1 < candidate else candidate
+#     D[t] = candidate
+#     t += 1
+#
+# print(D[N])
 
-N = int(input())
-D = [0] * (N + 1)
-t = 2
+def dp(n):
+    if n in D:
+        return D[n]
+    D[n] = min(dp(n//2) + n % 2, dp(n // 3) + n % 3) + 1
+    return D[n]
 
-while t <= N:
-    candidate = 1000
-    if t % 3 == 0:
-        candidate = D[t // 3] + 1 if D[t // 3] + 1 < candidate else candidate
-    if t % 2 == 0:
-        candidate = D[t // 2] + 1 if D[t // 2] + 1 < candidate else candidate
-    if D[t-1]:
-        candidate = D[t - 1] + 1 if D[t - 1] + 1 < candidate else candidate
-    D[t] = candidate
-    t += 1
-
-print(D[N])
-
-
+D = {1:0, 2:1}
+n = int(input())
+print(dp(n))
+print(D)

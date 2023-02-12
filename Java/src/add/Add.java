939137bfc527edbe28f8c3b8c52e4687e8691f73
cheol-95 <rkdcjf0122@gmail.com>
2023-02-13 00:51:09 +0900

    001. 더하기

---
+++ Java/src/add/Add.java
@@ -0,0 +1,12 @@
+package add;
+
+public class Add {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println(add(a, b));
+    }
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+}
